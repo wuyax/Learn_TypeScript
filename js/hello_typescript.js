edfe485d2f67ae86f6c43627fd9f8e79350c1164
wuyax <pengqingkai@gmail.com>
2018-01-02 09:58:25 +0800

    hello typescript

---
+++ js/hello_typescript.js
@@ -0,0 +1,3 @@
+var str1 = 'hello';
+var str2 = 'typescript';
+document.write(str1 + str2);
--- ts/hello_typescript.ts
+++ ts/hello_typescript.ts
@@ -1,3 +1,3 @@
-let str1:string = 'hello'
-let str2:string = 'typescript'
+let str1: string = 'hello'
+let str2: string = 'typescript'
 document.write(str1 + str2)
